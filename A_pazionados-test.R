4e347c4d5f16c7d1a577720c415b213e50a6d3b8
collinschwantes <cjschwantesll@gmail.com>
2015-09-01 15:21:01 -0400

    removed spaces from the file to test commit in Rstudio

--- A_pazionados-test.R
+++ A_pazionados-test.R
@@ -42,8 +42,6 @@ tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
       )               
   )
 
-
-
 server <- function(input, output) {
   df <- read.csv(file = "/Users/featherlite569/Documents/La Pazionado/La Pazionado/Apazionados.csv")
   df <- cbind(df, geocode(as.character(df$City), output = "more")) 
